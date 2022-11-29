ed4711457400e3618849e0cea8e55879e86c400d
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2022-11-29 20:04:04 -0300

    Update streamliit.py

--- streamliit.py
+++ streamliit.py
@@ -8,7 +8,6 @@ import Contabilidad as co
 st.write('''
 ## `Gestión Kurt Dic 22`
 ''')
-st.markdown("<h4 style='text-align: justify;'>Carga de Compras, en el 2do la estadia de cada uno, el 3ro veras si debes poner o recibir plata y el 4to es solo para el dto. contable donde imputa el precio de lo consumido en el dia.</h1>", unsafe_allow_html=True)
 
 
 selected = option_menu(None, ["Gastos", "Estadia", "Resumen", 'Dto. Contable'],
